72c56294d5a92c51c3ef9c661258fbe0ff2264aa
Jean <jeanhepe@hotmail.com>
2024-01-10 00:06:29 +0000

    first commit

---
+++ udagram/.ipynb_checkpoints/set_env-checkpoint.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres17
+export POSTGRES_HOST=udacity.ctcxdph7oxfj.eu-central-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::udacity-deployment
+export AWS_REGION=eu-central-1
+export AWS_PROFILE=default
+export JWT_SECRET=mysecretstring
+export URL=http://localhost:8100
